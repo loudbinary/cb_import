24ef9f4b414f8ed63ff7994d3403c80f83af7d4a
Charles Russell <charles.russell@WTCH02112013030.local>
2017-05-12 06:04:32 -0400

    Modifications to help out a little

--- index.js
+++ index.js
@@ -3,33 +3,53 @@ var fs = require('fs'),
     es = require('event-stream'),
     fs = require('fs'),
     uuidV1 = require('uuid/v1'),
-    path = require('path');
+    path = require('path'),
+    async = require('async');
 var getStream = function () {
     var jsonData = 'flight_restrictions.json',
         stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
         parser = JSONStream.parse('*');
     return stream.pipe(parser);
 };
-
-fs.mkdir(path.join(__dirname,'export'), function(err){
-    if (err) console.log('Export directory existed, continuing')
-    else {
-        console.log('Export direct created continuing...');
+async.series([
+    function(callback){
+        fs.mkdir(path.join(__dirname,'import'), function(err) {
+            if (err) console.log('Export directory existed, continuing')
+            else {
+                console.log('Export directory created, continuing');
+            }
+            callback(null);
+        })
+    },
+    function(callback){
+        fs.mkdir(path.join(__dirname,'import','docs'), function(err) {
+            if (err) console.log('Docs directory existed, continuing')
+            else {
+                console.log('Docs directory created, continuing');
+            }
+            callback(null);
+        })
+    },
+    function(callback){
+        run(function(err){
+            console.log('Export completed');
+        })
     }
-    run();
-})
+])
 
-function run() {
+
+function run(callback) {
     getStream()
         .pipe(es.mapSync(function (data) {
             if (Array.isArray(data)) {
 
                 data.forEach(function (item) {
-                    let file = path.join(__dirname, 'export', uuidV1() + '.json');
+                    let file = path.join(__dirname, 'import', uuidV1() + '.json');
                     fs.writeFileSync(file, JSON.stringify(item), 'utf-8');
                 })
 
             }
 
         }));
+    callback(null);
 }
