fd0bd422d67043cae3a9ce410b30ad9eb83fad39
Charles Russell <charles.russell@webteks.com>
2017-05-15 10:29:54 -0400

    Working code, verify simple import through gateway of a couchbase backup

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Import Couchbase backup into new Cluster
+
+To run this code, you must create a .env in the root of cloned repository.
+
+Example .env
+
+POST_URL="http://mycouchbase-servers-gateway-sync-url"
+POST_PORT=4984
+FILE_RESTORE_NAME="flight_restrictions.json"
+DB_JSON_NODE="flight-restrictions"
+DB_NAME="facilities"
+
+Once you've create the file, you are then able to take a backup of your local Couchbase server, via UI.  This will
+produce a zip file.  You will need to extract the contents of the zip file into directory where repository is cloned.
+
+We only need the .json file.
+
+Once you have extracted the json file, you will need to identify the JSON object to be imported.
+
+Once completed, and ready, then just execute
+
+node index.js
+
+
+
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/*
-    Within the root of cloned repository, create a .env file
-    Within that file:
-        POST_URL="HTTP://COUCHBASE-GATEWAY-SYNC-URL"
-        DB_NAME="COUCHBASE GATEWAY SYNC DB NAME"
- */
 require('dotenv').config();
 var fs = require('fs'),
     JSONStream = require('JSONStream'),
@@ -14,15 +8,16 @@ var fs = require('fs'),
     async = require('async'),
     http = require("http"),
     request = require("request"),
+    sleep = require('sleep',)
     options = {
         keepAlive:true,
         maxSockets:10
     };
 
 var agent = new http.Agent(options);
-
+var postUrl = process.env.POST_URl + ':' + process.env.POST_PORT + '/' + process.env.DB_NAME + '/'
 options = {
-    url: process.env.POST_URL + ':' + '4984' + process.env.DB_NAME,
+    url: postUrl,
     headers: {
         "content-type": "application/json"
     },
@@ -30,53 +25,26 @@ options = {
     agent: agent
 };
 var getStream = function () {
-    var jsonData = 'flight_restrictions.json',
+    var jsonData = process.env.FILE_RESTORE_NAME,
         stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
         parser = JSONStream.parse('*');
     return stream.pipe(parser);
 };
 getStream()
     .pipe(es.mapSync(function (data) {
+        var count = 0;
         if (Array.isArray(data)) {
+            console.log('Beginning import of', data.length, ' json objects into')
 
             data.forEach(function (item) {
-                options.body = JSON.stringify(item['flight-restrictions']);
-                var cbPost = http.request(options);
-                cbPost.end();
-                cbPost.on("response", function(incomingMessage){
-                    incomingMessage.on("readable", function(){
-                        var message = incomingMessage.read();
-                    });
-                });
+                options.body = JSON.stringify(item[process.env.DB_JSON_NODE]);
+                request.post(options,function(error,response,body){
+                    if (error) console.log(error);
+                    count++
+                    console.log('Imported item', count);
+                })
             })
 
         }
 
     }));
-/*
- var request = require("request");
-
- var options = { method: 'POST',
- url: 'http://couchbase-gateway-sync-1473293852.us-east-1.elb.amazonaws.com:4984/facilities/',
- headers:
- { 'postman-token': 'e49629ea-37ce-2286-d050-474731e84af3',
- 'cache-control': 'no-cache',
- 'content-type': 'application/json' },
- body:
- { Category: 'HELIPORT',
- Classification: '',
- Contact: { Name: 'SABEY CORPORATION', Phone: '    206-281-8700' },
- Name: 'ELLIOTT PARK',
- NotamsId: '',
- Owner: 'SABEY CORPORATION',
- Shape: { Points: [ 47.61982027777778, -122.3606825 ] },
- TimePeriod: { UtcEnd: '', UtcStart: '' } },
- json: true };
-
- request(options, function (error, response, body) {
- if (error) throw new Error(error);
-
- console.log(body);
- });
-
- */
